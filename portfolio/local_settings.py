264d35875f23a71996054db3c74fdc04dc080107
MetaPoly“ <ksoakey@gmail.com>
2021-04-10 08:13:30 -0700

    “dj12

---
+++ portfolio/local_settings.py
@@ -0,0 +1,11 @@
+DATABASES ={
+    'Default'
+        'ENGINE: 'django.db.backends.postgresql',
+        'NAME: 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'django1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
+ }
+
